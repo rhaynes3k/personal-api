6102d3b41e81aa7b865ef39267fce844885743ac
rhaynes3k <rhaynes3k@gmail.com>
2016-06-18 20:56:52 -0500

    addedRoutesFile

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {json} = require('body-parser');
 const middleware = require('./controllers/middleware');
 const mainCtrl = require('./controllers/mainCtrl');
+const routes = require('./controllers/routes');
+
 const app = express();
 const port = 8900;
 
@@ -9,28 +11,6 @@ const port = 8900;
 app.use(json());
 app.use(middleware.addHeaders);
 
-// GETS
-
-// ****** VERIFY USER BEFORE ACCESS ******
-app.get('/secrets/:username/:pin', middleware.verifyUser, mainCtrl.getSecrets);
-// ****** VERIFY USER BEFORE ACCESS ******
-app.get('/name', mainCtrl.getName);
-app.get('/location', mainCtrl.getLoc);
-app.get('/occupations', mainCtrl.getoccs);
-app.get('/occupations/latest', mainCtrl.getoccsLat);
-app.get('/hobbies', mainCtrl.getHobbies);
-
-app.get('/skills', mainCtrl.getSkls);
-
-// PUTS
-app.put('/name', mainCtrl.chgName);
-app.put('/location', mainCtrl.updtLoc);
-
-// POSTS
-app.post('/hobbies', mainCtrl.addHobbies);
-app.post('/occupations', mainCtrl.addoccs);
-
-app.post('/skills', middleware.generateId, mainCtrl.addSkills);
 
 
 
